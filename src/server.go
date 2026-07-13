780575a0b7de0564dc83c52316b706154f4d9457
ArjavTripathi <arjavtri@iastate.edu>
2026-07-12 21:53:56 -0500

    started redis

---
+++ src/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	PORT := os.Getenv("PORT")
+	fmt.Printf("Listeneing on port %s", PORT)
+
+	listener, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		fmt.Printf("Error listening on port %s: %v", PORT, err)
+		return
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Printf("Error accepting connection: %v", err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+
+		_, err = conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading from connection: %v", err)
+			os.Exit(1)
+		}
+
+		conn.Write([]byte("OK\r\n"))
+	}
+}
